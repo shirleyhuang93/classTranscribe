51fc5558675fbb0d4ad7cbd9b72b37e0d1a320f7
Shirley Huang <Shirley@wirelessprv-10-195-229-211.near.illinois.edu>
2017-04-13 10:16:35 -0500

    tried to make link work

--- server.js
+++ server.js
@@ -48,6 +48,16 @@ app.get('/', function (request, response) {
   response.end(html);
 });
 
+var loginHomePage = fs.readFileSync(mustachePath + 'dashboard.mustache').toString();
+app.get('/', function (request, response) {
+  response.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+
+  var html = Mustache.render(loginHomePage);
+  response.end(html);
+});
+
 var viewerMustache = fs.readFileSync(mustachePath + 'viewer.mustache').toString();
 app.get('/viewer/:className', function (request, response) {
   var className = request.params.className.toLowerCase();
